f5d0f31837d65294cbfe063eaf3d8f49c6164afa
Ma <mattiadanese@MacBook-Air-2.attlocal.net>
2023-07-26 00:29:51 -0400

    CS 118

---
+++ CS 118 - Cloud Computing/Creative Final Project/serverless-site/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_PtJK03aTT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3n1qhmq7bkqgaf8b8q5qbqh0do', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://ugglflyds3.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
